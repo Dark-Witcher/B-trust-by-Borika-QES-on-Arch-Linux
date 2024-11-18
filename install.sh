beb9c72b555858070c6b9c44635f51f4eee9dc22
Dark Witcher <Dark-Witcher@users.noreply.github.com>
2024-11-19 01:59:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ yay -Sy sac-core sac-gui btrustbiss
 #Add Gemelto PKCS11 to the security library
 mkdir $HOME/.pki && mkdir $HOME/.pki/nssdb
 
-modutil -add "PKCS11_Gemalto" -libfile libIDPrimePKCS11.so -dbdir sql:$HOME/.pki/nssdb -mechanisms FRIENDLY
+modutil -add "libIDPrimePKCS11.so" -libfile libIDPrimePKCS11.so -dbdir sql:$HOME/.pki/nssdb -mechanisms FRIENDLY
+
+systemctl enable pcscd.service
+
+systemctl start pcscd.service
 
 echo "Installation completed! Follow instructions for browser support!"
