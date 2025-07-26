ebb84faf354cddb3fafd3fd818c7f2e796c93f7a
Dark Witcher <Dark-Witcher@users.noreply.github.com>
2025-07-26 14:27:28 +0300

    Update install.sh
    1. Added a function to check if "paru" or "yay" are installed on the system, and if they aren't, to install "yay".
    
    2. Added a function to check if "paru" or "yay" are present and to use the one present to install sac-core, sac-gui and btrustbiss.
    
    3. Added a step to copy BISS desktop icon and place it in /usr/share/applications

--- install.sh
+++ install.sh
@@ -3,8 +3,30 @@
 #Install the Smart Security Card Reader Tools:
 sudo pacman -Sy opensc ccid pcsc-tools pcsclite
 
-#Install SafetyNet Authentication and B-trust Biss:
-yay -Sy sac-core sac-gui btrustbiss
+#Checking if yay or paru is installed and installing yay if neither is present:
+if ! command -v paru >/dev/null 2>&1 && ! command -v yay >/dev/null 2>&1; then
+    echo "paru and yay not found. Installing yay..."
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd -
+    rm -rf /tmp/yay
+    echo "YAY is installed to be used for AUR package installs."
+else
+    echo "AUR installer present."
+fi
+
+#Install SafetyNet Authentication and B-trust Biss from AUR based on paru or yay present:
+if command -v paru >/dev/null 2>&1; then
+    paru -Sy sac-core sac-gui btrustbiss
+elif command -v yay >/dev/null 2>&1; then
+    yay -Sy sac-core sac-gui btrustbiss
+else
+    echo "Something went wrong. We can't locate AUR package installer."
+    exit 1
+fi
+
+sudo cp /opt/btrustbiss/lib/btrustbiss-BISS.desktop /usr/share/applications/
 
 #Add Gemelto PKCS11 to the security library
 if [ ! -d "$HOME/.pki" ]; then
@@ -17,8 +39,8 @@ fi
 
 modutil -add "libIDPrimePKCS11.so" -libfile libIDPrimePKCS11.so -dbdir sql:$HOME/.pki/nssdb -mechanisms FRIENDLY
 
-systemctl enable pcscd.service
+sudo systemctl enable --now pcscd.service
 
-systemctl start pcscd.service
+sudo systemctl status pcscd.service
 
 echo "Installation completed! Follow instructions for browser support!"
